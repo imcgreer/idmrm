b02fba27a262e9691686e2b582987f12630fbc3c
Ian McGreer <imcgreer@gmail.com>
2017-06-20 23:26:59 +0100

    starting work on catalogs

--- cfht/cfhtrm.py
+++ cfht/cfhtrm.py
@@ -4,11 +4,84 @@ import os
 import glob
 import numpy as np
 from astropy.table import Table
+import fitsio
+from bokpipe.bokdm import SimpleFileNameMap
 
-cfhtdir = os.environ.get('CFHTRMDATA',
+cfhtImgDir = os.environ.get('CFHTRMDATA',
                          os.path.join(os.environ['HOME'],'data','public',
                                       'CFHT','archive','SDSSRM'))
 
-def load_obs_table():
-	return Table.read(os.path.join(cfhtdir,'result_g5s2i0ftxwta9jbn.csv'))
+cfhtCatDir = os.environ.get('CFHTRMOUTDIR',
+                         os.path.join(os.environ['BOK90PRIMEOUTDIR'],'CFHT'))
+
+def set_obs_db_item(t,i,imFile):
+	f = fitsio.FITS(imFile)
+	h = f[0].read_header()
+	t['objName'][i] = h['OBJECT']
+	t['utDate'][i] = h['DATE-OBS'].replace('-','')
+	#t['date_obs'] = h['DATE-OBS']
+	t['utObs'][i] = h['UTC-OBS']
+	t['lstObs'][i] = h['LST-OBS']
+	t['mjdStart'][i] = h['MJD-OBS']
+	t['airmass'][i] = h['AIRMASS']
+	t['humidity'][i] = h['RELHUMID']
+	t['ccdbin1'][i] = h['CCDBIN1']
+	t['ccdbin2'][i] = h['CCDBIN2']
+	try:
+		t['elixirZp'][i] = h['PHOT_C']
+	except:
+		pass
+	for j,hdu in enumerate(f[1:]):
+		h = hdu.read_header()
+		t['ccdGain'][i,j] = h['GAIN']
+		t['ampGain'][i,j] = [ h['GAINA'], h['GAINB'] ]
+
+def make_obs_db():
+	arxDat = Table.read(os.path.join(cfhtImgDir,
+	                                 'result_g5s2i0ftxwta9jbn.csv'))
+	t = Table()
+	t['expNum'] = arxDat['Sequence Number']
+	t['fileName'] = np.char.strip(arxDat['Product ID'])
+	t['utDate'] = 'YYYYMMDD'
+	t['imType'] = 'object'
+	t['filter'] = 'g'
+	t['longFilt'] = arxDat['Filter']
+	t['objName'] = ' '*35
+	t['expTime'] = arxDat['Int. Time']
+	t['ccdbin1'] = np.int32(0)
+	t['ccdbin2'] = np.int32(0)
+	t['airmass'] = np.float32(0.0)
+	t['utObs'] = 'HH:MM:SS.SSS'
+	t['lstObs'] = 'HH:MM:SS.SS'
+	t['targetRa'] = arxDat['RA (J2000.0)']
+	t['targetDec'] = arxDat['Dec. (J2000.0)']
+	t['mjdStart'] = arxDat['Start Date'].astype(np.float64)
+	t['humidity'] = np.float32(0.0)
+	t['ccdGain'] = np.zeros((1,36),dtype=np.float32)
+	t['ampGain'] = np.zeros((1,36,2),dtype=np.float32)
+	t['elixirZp'] = np.float32(0.0)
+	for i,fn in enumerate(t['fileName']):
+		try:
+			set_obs_db_item(t,i,os.path.join(cfhtImgDir,fn+'.fits.fz'))
+		except IOError:
+			continue
+	t.sort('expNum')
+	t.write(os.path.join('.','config','sdssrm-cfht.fits.gz'),
+	        overwrite=True)
+
+class CfhtObsDb(object):
+	def __init__(self):
+		cfhtDir = '.' # XXX
+		self.obsDb = Table.read(os.path.join(cfhtDir,'config',
+		                                     'sdssrm-cfht.fits.gz'))
+		if not os.path.exists(cfhtCatDir):
+			os.makedirs(cfhtCatDir)
+		self.fmap = { ftype:SimpleFileNameMap(cfhtImgDir,cfhtCatDir,'.'+ftype)
+		                for ftype in ['cat','wcscat','psf'] }
+		self.fmap['img'] = SimpleFileNameMap(cfhtImgDir,cfhtCatDir,
+		                                     fromRaw=True)
+	def getFiles(self,filt=None):
+		return self.obsDb['fileName']
+	def __call__(self,ftype):
+		return self.fmap[ftype]
 
--- cfht/cfhtrmphot.py
+++ cfht/cfhtrmphot.py
@@ -2,6 +2,7 @@
 
 import os
 import numpy as np
+import subprocess
 import multiprocessing
 from functools import partial
 from astropy.io import fits
@@ -11,11 +12,63 @@ from astropy.wcs import InconsistentAxisTypesError
 
 from bokpipe import bokphot,bokpl,bokproc,bokutil
 import bokrmpipe
+import cfhtrm
 
 # Nominal limits to classify night as "photometric"
 zp_phot_nominal = {'g':25.90,'i':25.40}
 
 nom_pixscl = 0.18555
 
-cfhtrm_aperRad = np.array([0.8,1.6,2.4,3.2,4.0,7.0,10.]) / nom_pixscl
+cfhtrm_aperRad = np.array([0.75,1.5,2.275,3.4,4.55,6.67,10.]) / nom_pixscl
+
+def _cat_worker(dataMap,imFile,**kwargs):
+	bokutil.mplog('extracting catalogs for '+imFile)
+	import pdb; pdb.set_trace()
+	imgFile = dataMap('img')(imFile)
+	psfFile = dataMap('psf')(imFile)
+	tmpFile = imgFile.replace('.fz','')
+	if not os.path.exists(psfFile):
+		catFile = dataMap('wcscat')(imFile)
+		print '-->',imgFile
+		subprocess.call(['funpack',imgFile])
+		bokphot.sextract(tmpFile,catFile,full=False,**kwargs)
+		bokphot.run_psfex(catFile,psfFile,instrument='cfhtmegacam',**kwargs)
+		if False:
+			os.remove(catFile)
+	catFile = dataMap('cat')(imFile)
+	if not os.path.exists(catFile):
+		if not os.path.exists(tmpFile):
+			subprocess.call('funpack',imgFile)
+		bokphot.sextract(tmpFile,catFile,psfFile,full=True,**kwargs)
+	if os.path.exists(tmpFile):
+		os.remove(tmpFile)
+
+def make_sextractor_catalogs(dataMap,**kwargs):
+	procmap = map # XXX
+	apers = ','.join(['%.2f'%a for a in cfhtrm_aperRad])
+	kwargs.setdefault('DETECT_MINAREA','10.0')
+	kwargs.setdefault('DETECT_THRESH','2.0')
+	kwargs.setdefault('ANALYSIS_THRESH','2.0')
+	kwargs.setdefault('PHOT_APERTURES',apers)
+	kwargs.setdefault('SEEING_FWHM','1.0')
+	kwargs.setdefault('PIXEL_SCALE','0.18555')
+	kwargs.setdefault('SATUR_KEY','SATURATE')
+	kwargs.setdefault('GAIN_KEY','GAIN')
+	files = dataMap.getFiles()
+	files = [files[0]]
+	print files
+	p_cat_worker = partial(_cat_worker,dataMap,**kwargs)
+	status = procmap(p_cat_worker,files)
+
+if __name__=='__main__':
+	import sys
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--catalogs',action='store_true',
+	                help='make source extractor catalogs and PSF models')
+	args = parser.parse_args()
+	#
+	dataMap = cfhtrm.CfhtObsDb()
+	if args.catalogs:
+		make_sextractor_catalogs(dataMap)
 
